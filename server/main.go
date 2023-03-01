d75c2c72eec80cf7755ac4a9bf07a503b2cf2619
Christopher Mullen <camullen@usc.edu>
2023-02-28 22:12:35 -0800

    added some log messages in main printing state

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"server/controller"
 	"server/model"
 )
@@ -11,8 +13,13 @@ const dbName = "api_challenge_db"
 const collectionName = "notesCollection"
 
 func main() {
+	log.Printf("Connecting to MongoDB with URI: %v\n", mongoURI)
 	model.InitDB(mongoURI)
 	defer model.CloseDB()
+
+	log.Println("Initializing notes collection...")
 	model.InitNotes(dbName, collectionName)
+
+	log.Println("Initializing server...")
 	controller.Start(port)
 }
